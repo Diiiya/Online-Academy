ced91c4f34ea1274fbf16a57e11fbd24afbe105b
d.boyadzhieva <d.boyadzhieva@BOYADZHIEVAD01.seeburger.de>
2019-06-25 16:33:58 +0300

    Login method that checks: 1. If all fields are filled in 2. If the username exists 3. If the combination of username and password is correct

--- onlineAcademy/src/main/java/com/academy/onlineAcademy/view/LoginView.java
+++ onlineAcademy/src/main/java/com/academy/onlineAcademy/view/LoginView.java
@@ -2,27 +2,39 @@ package com.academy.onlineAcademy.view;
 
 import java.io.File;
 
+import com.academy.onlineAcademy.controller.PersonController;
+import com.academy.onlineAcademy.model.Person;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
 import com.vaadin.server.FileResource;
+import com.vaadin.server.Page;
 import com.vaadin.server.VaadinService;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 public class LoginView extends VerticalLayout implements View {
 	
+	String databaseUsername;
+	String enteredUsername;
+	String databasePassword;
+	String enteredPassword;
+	PersonController obj = new PersonController();
+	Person currentUser = new Person();
+	
 	final VerticalLayout layout = new VerticalLayout();
 			String basepath = VaadinService.getCurrent().getBaseDirectory().getAbsolutePath();
 			FileResource resource = new FileResource(new File(basepath +
 		            "/1024px-Circle-icons-profile.svg.png"));
 			Image image = new Image("", resource);
-			final TextField usernameField = new TextField();
-			final TextField passwordField = new TextField();
+			TextField usernameField = new TextField("Type your username:");
+			PasswordField passwordField = new PasswordField("Type your password:");
 			Button loginButton = new Button("Login");
 			Button signUpButton = new Button("Sign up");
 			Navigator navigator = UI.getCurrent().getNavigator();
@@ -31,18 +43,25 @@ public class LoginView extends VerticalLayout implements View {
 		
 		image.setWidth("100px");
 		image.setHeight("100px");
-	    
-	    usernameField.setCaption("Type your username:");    
+	       
 	    usernameField.setWidth("200px");
-	    passwordField.setCaption("Type your password:");
 	    passwordField.setWidth("200px");
 	    loginButton.setWidth("200px");
+	    
 	    loginButton.addClickListener(e -> {
-	        // 1. Checks user name and password combination
-	    	// 2. If true -> navigates to the user's (with id) courses page
-	    	navigator.navigateTo("UserCourses");
-	    	// 3. If false -> displays a wrong input combination message
+	    	enteredUsername = usernameField.getValue().toUpperCase();
+		    enteredPassword = passwordField.getValue();
+		    if (((enteredUsername.length() != 0) && (enteredUsername != "")) && ((passwordField.getValue().length() != 0) && (passwordField.getValue() != ""))) {
+		          checkIfTheUsernameExists();
+		          checkUsernamePasswordCombination();
+		    }
+		    else {
+		    	Notification notif = new Notification("Warning", "Both fields should be filled in!",
+					    Notification.TYPE_WARNING_MESSAGE);
+		    	notif.show(Page.getCurrent());
+		    }
 	    });
+	    
 	    signUpButton.setWidth("200px");
 	    signUpButton.addClickListener(e -> navigator.navigateTo("SignUp"));
 	    
@@ -57,5 +76,27 @@ public class LoginView extends VerticalLayout implements View {
 	    addComponent(layout);
 		
 	}
-
+	
+	public void checkIfTheUsernameExists() {
+		try {		    	
+	    	currentUser = obj.getPersonByUsername(enteredUsername);
+	     }
+		 catch (Exception ex) {
+			 Notification notif = new Notification("Warning", "The username doesn't exist! Please use another one or create an account!",
+					    Notification.TYPE_WARNING_MESSAGE);
+				notif.show(Page.getCurrent());
+		 }
+	}
+	
+	public void checkUsernamePasswordCombination() {
+		if (currentUser.getPassword().equals(enteredPassword)) {
+    		navigator.navigateTo("UserCourses" + "/" + currentUser.getId());
+    	}
+    	else {
+    		Notification notif = new Notification("Warning", "The combination of username and password is not correct!",
+				    Notification.TYPE_WARNING_MESSAGE);
+			notif.show(Page.getCurrent());
+    	}
+	}
+	
 }
